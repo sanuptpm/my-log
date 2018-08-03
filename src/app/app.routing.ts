7acdb9990a512c395032c60d321ec4ee4fbfdda6
Sanu MN <sanu.mn@pitsolutions.com>
2018-08-03 17:25:40 +0530

    login with design

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,10 +3,10 @@
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 // import { RegisterComponent } from './register';
-// import { AuthGuard } from './_guards';
+import { AuthGuard } from './_guards/auth.guard';
 
 const appRoutes: Routes = [
-    // { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
     { path: '', component: LoginComponent },
     { path: 'login', component: LoginComponent },
     { path: 'home', component: HomeComponent },
